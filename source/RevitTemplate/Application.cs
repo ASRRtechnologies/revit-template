d95138d3d7c8c81f53656693b025f8d1c43c435a
Julius Cathalina <julius.cathalina@gmail.com>
2024-07-26 02:06:51 +0200

    feat(init): initial commit

---
+++ source/RevitTemplate/Application.cs
@@ -0,0 +1,23 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using RevitTemplate.Commands;
+
+namespace RevitTemplate;
+
+[UsedImplicitly]
+public class Application : ExternalApplication
+{
+    public override void OnStartup()
+    {
+        Host.Start();
+        CreateRibbon();
+    }
+
+    private void CreateRibbon()
+    {
+        var panel = Application.CreatePanel("Commands", "ASRR");
+
+        panel.AddPushButton<PopupCommand>("Click Me!")
+            .SetImage("/RevitTemplate;component/Resources/Icons/RibbonIcon16.png")
+            .SetLargeImage("/RevitTemplate;component/Resources/Icons/RibbonIcon32.png");
+    }
+}
---
+++ source/RevitTemplate/Commands/PopupCommand.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Nice3point.Revit.Toolkit.External;
+
+namespace RevitTemplate.Commands;
+
+[UsedImplicitly]
+[Transaction(TransactionMode.Manual)]
+public class PopupCommand : ExternalCommand
+{
+    public override void Execute()
+    {
+        try
+        {
+            TaskDialog.Show("ASRR Sample Add-in", "If you see this, the add-in works!");
+        }
+        catch (Exception e)
+        {
+            TaskDialog.Show("ASRR Sample Add-in", $"Exception found: {e.Message}");
+        }
+    }
+}
