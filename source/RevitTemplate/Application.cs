ac3f28e921dbe3db30b66703720f0eedd3ffa5f3
Vanishaov <V.OedayrajsinghVarma@student.tudelft.nl>
2024-10-25 02:08:03 +0200

    Add buttons for settings and organize ribbon panel

--- source/RevitTemplate/Application.cs
+++ source/RevitTemplate/Application.cs
@@ -1,5 +1,9 @@
-﻿using Nice3point.Revit.Toolkit.External;
+﻿using System.Reflection;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI;
+using Nice3point.Revit.Toolkit.External;
 using RevitTemplate.Commands;
+using RevitTemplate.Commands.Settings;
 
 namespace RevitTemplate;
 
@@ -14,14 +18,48 @@ public class Application : ExternalApplication
 
     private void CreateRibbon()
     {
-        var panel = Application.CreatePanel("Commands", "ASRR");
+        var testPanel = Application.CreatePanel("Test", "ASRR");
 
-        panel.AddPushButton<PopupCommand>("Click Me!")
+        testPanel.AddPushButton<PopupCommand>("Click Me!")
             .SetImage("/RevitTemplate;component/Resources/Icons/RibbonIcon16.png")
             .SetLargeImage("/RevitTemplate;component/Resources/Icons/RibbonIcon32.png");
-        
-        panel.AddPushButton<PlaceWallCommand>("Place Wall");
 
-        panel.AddPushButton<ConfigureFacadeCommand>("Configure Facade");
+        var settingsPanel = Application.CreatePanel("Settings", "ASRR");
+        var dbSettingsButton = CreatePushButtonData<OpenDatabaseSettingsCommand>(
+            "dbSettingsButton",
+            "Database Settings",
+            "Open database settings",
+            "/RevitTemplate;component/Resources/Icons/RibbonIcon16.png"
+        );
+        var exportSettingsButton =
+            CreatePushButtonData<OpenExportSettingsCommand>(
+                "exportSettingsButton",
+                "Export Settings",
+                "Open export settings",
+                "/RevitTemplate;component/Resources/Icons/RibbonIcon16.png"
+            );
+        settingsPanel.AddStackedItems(dbSettingsButton, exportSettingsButton);
+
+        var commandsPanel = Application.CreatePanel("Commands", "ASRR");
+        commandsPanel.AddPushButton<PlaceWallCommand>("Place Wall").ToolTip = "Place wall";
+        commandsPanel.AddPushButton<ConfigureFacadeCommand>("Configure Facade").ToolTip = "Configure facade by id";
+    }
+
+    private static PushButtonData CreatePushButtonData<T>(string name, string text, string toolTip = null,
+        string imagePath = null, string largeImagePath = null)
+    {
+        var type = typeof(T);
+        var fullName = type.FullName;
+        return new PushButtonData(
+            name,
+            text,
+            Uri.UnescapeDataString(new UriBuilder(Assembly.GetAssembly(type).CodeBase).Path),
+            fullName
+        )
+        {
+            ToolTip = toolTip,
+            Image = imagePath != null ? new BitmapImage(new Uri(imagePath, UriKind.Relative)) : null,
+            LargeImage = largeImagePath != null ? new BitmapImage(new Uri(largeImagePath, UriKind.Relative)) : null
+        };
     }
 }
---
+++ source/RevitTemplate/Commands/Settings/OpenExportSettingsCommand.cs
@@ -0,0 +1,18 @@
+﻿using ASRR.Core.Persistence;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using RevitTemplate.Settings;
+
+namespace RevitTemplate.Commands.Settings;
+
+[UsedImplicitly]
+[Transaction(TransactionMode.Manual)]
+public class OpenExportSettingsCommand : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        var persistentStorageProvider = Host.GetService<IPersistentStorageProvider>();
+        persistentStorageProvider.Open<ExportSettings>();
+        return Result.Succeeded;
+    }
+}
